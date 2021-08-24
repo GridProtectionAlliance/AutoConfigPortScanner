212045486af3114c4bcc25057da00f0cb808eaf0
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2021-08-24 00:52:57 -0400

    Added global warning supressions

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,43 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonImport_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonScan_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.ExecuteScan(AutoConfigPortScanner.ScanParameters,System.Threading.CancellationToken)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.RequestDeviceConfiguration(System.String,System.Int32,AutoConfigPortScanner.ScanParameters,System.Threading.CancellationToken)~GSF.PhasorProtocols.IConfigurationFrame")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.SaveDeviceConfiguration(GSF.PhasorProtocols.IConfigurationFrame,System.Int32,System.Int32,AutoConfigPortScanner.ScanParameters)~System.Boolean")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonBrowseIDCodes_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.ScanParameters.InitializePointTagExpressionParser~GSF.Parsing.TemplatedExpressionParser")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~P:AutoConfigPortScanner.ScanParameters.CompanyAcronym")]
+[assembly: SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "<Pending>", Scope = "member", Target = "~P:AutoConfigPortScanner.ScanParameters.CompanyID")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonBrowseConfig_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonCancel_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonImport_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonScan_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonSettings_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.linkLabelClearConsole_LinkClicked(System.Object,System.Windows.Forms.LinkLabelLinkClickedEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.m_frameParser_BufferParsed(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.m_frameParser_ConnectionAttempt(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.m_frameParser_ConnectionEstablished(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.m_frameParser_ConnectionException(System.Object,GSF.EventArgs{System.Exception,System.Int32})")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.m_frameParser_ConnectionTerminated(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.m_frameParser_ExceededParsingExceptionThreshold(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.m_frameParser_ParsingException(System.Object,GSF.EventArgs{System.Exception})")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.m_frameParser_ReceivedConfigurationFrame(System.Object,GSF.EventArgs{GSF.PhasorProtocols.IConfigurationFrame})")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxEndComPort_Validated(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxEndComPort_Validating(System.Object,System.ComponentModel.CancelEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxEndIDCode_Validated(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxEndIDCode_Validating(System.Object,System.ComponentModel.CancelEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxIDCodeValues_TextChanged(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxIDCodeValues_Validated(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxIDCodeValues_Validating(System.Object,System.ComponentModel.CancelEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxStartComPort_Validated(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxStartComPort_Validating(System.Object,System.ComponentModel.CancelEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxStartIDCode_Validated(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.textBoxStartIDCode_Validating(System.Object,System.ComponentModel.CancelEventArgs)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:AutoConfigPortScanner.MainForm.buttonBrowseIDCodes_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Performance", "CA1819:Properties should not return arrays", Justification = "<Pending>", Scope = "member", Target = "~P:AutoConfigPortScanner.Settings.IDCodes")]
--- Model/Device.cs
+++ Model/Device.cs
@@ -1,7 +1,5 @@
 ﻿// ReSharper disable CheckNamespace
 
-#pragma warning disable 1591
-
 using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
